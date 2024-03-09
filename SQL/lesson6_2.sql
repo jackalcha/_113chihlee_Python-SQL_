0aee8cf8d7377baade6049379880d79ae2e60f64
張舜翔 <jackalcha@gmail.com>
2024-03-09 15:09:31 +0800

    Update lesson6_2.sql

--- SQL/lesson6_2.sql
+++ SQL/lesson6_2.sql
@@ -5,9 +5,9 @@ SELECT 日期,進站人數,出站人數,名稱,地名,地址,youbike
 FROM gate_count LEFT JOIN stations ON 站點編號=編號;
 
 /*取出基隆市有那些火車站穩*/
-SELECT DISTINCT 名稱
+SELECT DISTINCT 日期,進站人數,出站人數,名稱,地名,地址,youbike
 FROM gate_count LEFT JOIN stations ON 站點編號=編號
-WHERE 地址 LIKE '高雄市%';
+WHERE 地址 LIKE '高雄%';
 
 /*取出基隆火車站2022年3月1日資料*/
 SELECT 日期,進站人數,出站人數,名稱,地名,地址,youbike
