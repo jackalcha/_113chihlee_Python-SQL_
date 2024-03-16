fae76341c217f54bd653e63e894294586c858c53
jackalcha <jackalcha@gmail.com>
2024-03-16 22:20:56 +0800

    Update having.sql

--- SQL/having.sql
+++ SQL/having.sql
@@ -2,17 +2,17 @@ SELECT *
 FROM gate_count LEFT JOIN stations ON 站點編號 = 編號
 
 /*全省各站點2022年進站總人數*/
-SELECT DATE_PART('year',日期) AS 年份,名稱,SUM(進站人數) AS 進站人數
+SELECT 名稱,SUM(進站人數) AS 進站人數
 FROM gate_count LEFT JOIN stations ON 站點編號 = 編號
 WHERE 日期 BETWEEN '2022-01-01' AND '2022-12-31'
-GROUP BY 年份,名稱
+GROUP BY 名稱
 ORDER BY 進站人數 DESC
 
 /*全省各站點2022年進站總人數大於5佰萬人的站點*/
-SELECT DATE_PART('year',日期) AS 年份,名稱,SUM(進站人數) AS 進站人數
+SELECT 名稱,SUM(進站人數) AS 進站人數
 FROM gate_count LEFT JOIN stations ON 站點編號 = 編號
 WHERE 日期 BETWEEN '2022-01-01' AND '2022-12-31'
-GROUP BY 年份,名稱
+GROUP BY 名稱
 HAVING SUM(進站人數) > 5000000
 
 /*基隆火車站2020年,每月份進站人數*/
