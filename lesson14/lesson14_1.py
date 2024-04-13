4ae82c9f65347cddec9657ca1064a46b323a518c
張舜翔 <124478041+jackalcha@users.noreply.github.com>
2024-04-13 06:24:27 +0000

    modify

--- lesson14/lesson14_1.py
+++ lesson14/lesson14_1.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.write('Hello, *World!* :sunglasses:')
 st.write('Hello, *World!* :sunglasses:')
+st.markdown('## 這是標題!')
+st.title("我是titile!")
+st.header("我是header")
+st.subheader("我是sub header")
