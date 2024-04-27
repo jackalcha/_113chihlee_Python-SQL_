dfb33438e65dffe0dc6d33fb89f9482c6b596e25
張舜翔 <jackalcha@gmail.com>
2024-04-27 16:10:08 +0800

    Update lesson18.sql

--- SQL/lesson18.sql
+++ SQL/lesson18.sql
@@ -41,7 +41,7 @@ FROM youbike
 GROUP BY 編號;
 
 /*取出最新時間youbike資料*/
-SELECT 日期,站點資訊.站點名稱,行政區,站點地址,lat,lng,總車輛,可借,可還,可借,活動
+SELECT 日期,站點資訊.站點名稱,行政區,站點地址,lat,lng,總車輪,可借,可還,可借,活動
 FROM youbike
 JOIN 站點資訊 ON youbike.編號 = 站點資訊.站點編號
 WHERE (日期,編號) IN (
